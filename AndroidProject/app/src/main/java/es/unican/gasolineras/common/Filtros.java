54da2b29b0ab3030d94ab99b3223fd73fd499187
noels <noelsolana2001@gmail.com>
2024-10-23 01:41:35 +0200

    Eliminación de un parámetro de entrada

--- AndroidProject/app/src/main/java/es/unican/gasolineras/common/Filtros.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/common/Filtros.java
@@ -12,12 +12,11 @@ public class Filtros {
     /**
      * Filters a list of gas stations by their state (open or closed)
      * @param gasolineras the list of gas stations to filter
-     * @param estado the state to filter by
      * @return the list of gas stations that match the filter
      * @throws DataAccessException if an error occurs while filtering
      */
 
-    public static List<Gasolinera> filtrarPorEstado(List<Gasolinera> gasolineras, boolean estado) throws DataAccessException {
+    public static List<Gasolinera> filtrarPorEstado(List<Gasolinera> gasolineras) throws DataAccessException {
         List<Gasolinera> resultado = new ArrayList<>();
 
         for (Gasolinera gasolinera : gasolineras) {
